cb965ae01af910a7e0795e3cf3b6733500c2a339
danialvaradob <danielalvarado@Daniels-MacBook-Pro.local>
2017-05-05 14:38:50 -0600

    update clase nodo supermercado

--- nodosupermercado.h
+++ nodosupermercado.h
@@ -2,12 +2,15 @@
 #define NODOSUPERMERCADO_H
 #include <arbolsupermercados.h>
 #include <arbolcategorias.h>
+#include <string>
 
 
 class NodoSupermercado
 {
 public:
-    NodoSupermercado();
+    NodoSupermercado(string _codSuper,string _codLugar, string _nombre );
+
+    void aumentarVentas() {cantidadVentas++;}
 
     NodoSupermercado *Hizq, *Hder;
     ArbolCategorias* Categorias;
@@ -15,7 +18,13 @@ public:
 
 
 private:
-    ArbolCategorias *_ArbolCategorias;
+    string  nombre;
+    string  codSuper;
+    string  codLugar;
+    int cantidadVentas;
+
+    friend class ArbolCategorias;
+    friend class ArbolSupermercados;
 };
 
 #endif // NODOSUPERMERCADO_H
