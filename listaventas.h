41cff5f3a39e02902dfdf71f69412599fd5071e6
danielalvarado <danialvarado@ic-itcr.ac.cr>
2017-05-10 17:03:12 -0600

    NodoVenta (medio updated)

--- listaventas.h
+++ listaventas.h
@@ -2,6 +2,11 @@
 #define LISTAVENTAS_H
 
 #include "nodoventa.h"
+#include "arbolcategorias.h"
+#include "arbolclientes.h"
+#include "arbolproductos.h"
+#include "arbolsupermercados.h"
+#include "arbolproveedores.h"
 #include <string>
 using namespace std;
 
@@ -11,6 +16,11 @@ public:
 
     void insertar(NodoVenta *_venta);
     bool listaVacia() {return primero == NULL;}
+    string getProveedorMasVentas(ArbolProveedores* _ListaProveedores);
+    string getClienteMasCompras(ArbolClientes * _ListaClientes);
+    string getProductoMasVendido(ArbolCategorias* _ListaCategorias);
+    string getCategoriaMasVendida(ArbolCategorias* _ListaCategorrias);
+
 
     NodoVenta* getPrimero() {return primero;};
 
--- nodoventa.h
+++ nodoventa.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 #include <string>
-#include "nodoproductocomprado.h"
 using namespace std;
 
 class NodoVenta {
